f64aa3e45c27228634ada00c712504e515a51af8
zenonet <78077158+zenonet@users.noreply.github.com>
2022-07-31 19:01:21 +0200

    Added a class for Extension-methods

---
+++ Zenonet.Utility/Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Zenonet.Utility;
+
+public static class Extensions
+{
+    /// <summary>
+    /// Finds all classes in the current process which derive from a type
+    /// </summary>
+    /// <param name="t">The type, all outputted types should derive from</param>
+    /// <returns>An array of all Types deriving from</returns>
+    public static Type[] GetAllInheritors(this Type t)
+    {
+        //Thanks to 'Yahoo Serious' for his answer here: https://stackoverflow.com/questions/857705/get-all-derived-types-of-a-type
+        return (from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+                from assemblyType in domainAssembly.GetTypes()
+                where t.IsAssignableFrom(assemblyType) && t != assemblyType
+                select assemblyType
+            ).ToArray();
+    }
+}
