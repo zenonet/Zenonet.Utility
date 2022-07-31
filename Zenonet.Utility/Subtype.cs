565aedfbad37a74a562471d0c92eee936e2e74c5
zenonet <78077158+zenonet@users.noreply.github.com>
2022-07-31 13:14:34 +0200

    Initial commit

---
+++ Zenonet.Utility/Subtype.cs
@@ -0,0 +1,35 @@
+﻿namespace Zenonet.Utility;
+
+/// <summary>
+/// A representation of a Type which ensures that the type derives from TBase
+/// </summary>
+/// <typeparam name="TBase">The base class, the type has to be assignable to</typeparam>
+public class Subtype<TBase>
+{
+    public Type Type;
+
+    public Subtype(Type type)
+    {
+        if (type.IsAssignableTo(typeof(TBase)))
+        {
+            Type = type;
+        }
+        else
+        {
+            throw new ArgumentException(
+                $"Can't create Subtype from {type.Name}" +
+                $" because it is not assignable to {typeof(TBase)}"
+            );
+        }
+    }
+
+    public static Subtype<TBase> Create<TType>() where TType : TBase
+    {
+        return new Subtype<TBase>(typeof(TType));
+    } 
+
+    public static implicit operator Type(Subtype<TBase> subtype)
+    {
+        return subtype.Type;
+    }
+}
